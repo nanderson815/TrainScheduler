8601d92fdc6c4a7c8a6d1cceebf4b594821c314e
Noah A <nanderson815@gmail.com>
2019-01-06 12:08:30 -0500

    Updated 1 6 19

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAlROg2XASMhlYpjDwBWkXRSwWhUBDDdHs",
+    authDomain: "trainscheduler-3c1f6.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3c1f6.firebaseio.com",
+    projectId: "trainscheduler-3c1f6",
+    storageBucket: "",
+    messagingSenderId: "286160758806"
+};
+firebase.initializeApp(config);
+
+var databse = firebase.database();
